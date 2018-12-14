5906819ad4a6a23b48a974dd631cab86ffe32d3a
Frode Danielsen <frode@e5r.no>
2018-12-14 12:29:19 +0100

    Set up Docker stack for easy local testing
    Summary:
    Adds a basic Docker stack config which runs a container with WordPress 5
    on PHP 7.2, with the plugin mounted from source. This enables easy
    testing of the widget on a clean install, and allows easy switching of
    WordPress and PHP versions for testing compatibility.

---
+++ bin/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SRC="${DIR}/src"
+
+echo "Installing composer dependencies ... "
+docker run --rm -ti -v $SRC:/app composer install --ignore-platform-reqs --no-scripts &>/dev/null
